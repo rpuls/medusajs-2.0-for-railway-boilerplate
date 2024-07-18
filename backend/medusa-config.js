b570678950e65cea433c6ac7f48781d2ea5522dc
Rasmus Puls <rasmus@triggerz.net>
2024-07-18 22:23:01 +0200

    added cors to medusa config

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,26 +1,38 @@
-import { loadEnv, defineConfig } from '@medusajs/utils';
+import { loadEnv, defineConfig } from '@medusajs/utils'
 
-loadEnv(process.env.NODE_ENV, process.cwd());
+loadEnv(process.env.NODE_ENV, process.cwd())
+
+const ADMIN_APP_PORT = process.env.PORT || 7001;
+const ADMIN_CORS = process.env.ADMIN_CORS || 'http://localhost:7000,http://localhost:7001'
+const STORE_CORS = process.env.STORE_CORS || 'http://localhost:8000'
 
 const plugins = [
+  'medusa-fulfillment-manual',
   {
-    resolve: "@medusajs/admin",
+    resolve: '@medusajs/admin',
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      path: "/",
+      autoRebuild: true,
+      develop: {
+        open: process.env.OPEN_BROWSER !== 'false',
+        port: ADMIN_APP_PORT,
+      },
     },
   }
-];
+]
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-});
+    redis_url: process.env.REDIS_URL,
+    database_url: process.env.DATABASE_URL,
+    database_type: 'postgres',
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+    auth_cors: process.env.AUTH_CORS,
+  },
+  plugins,
+  modules: {},
+  featureFlags: {
+    medusa_v2: true
+  },
+})
