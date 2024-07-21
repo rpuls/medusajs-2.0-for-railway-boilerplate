9eecb42a406d46cc85a67055da40e047f3c75957
Rasmus Puls <rasmus@triggerz.net>
2024-07-21 23:13:21 +0200

    config updated

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -15,17 +15,22 @@ const plugins = [
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  admin_cors: process.env.ADMIN_CORS,
-  auth_cors: process.env.AUTH_CORS,
-  store_cors: process.env.STORE_CORS,
-  jwtSecret: process.env.JWT_SECRET,
-  cookieSecret: process.env.COOKIE_SECRET,
+  http: {
+    adminCors: process.env.ADMIN_CORS,
+    authCors: process.env.AUTH_CORS,
+    storeCors: process.env.STORE_CORS,
+    jwtSecret: process.env.JWT_SECRET,
+    cookieSecret: process.env.COOKIE_SECRET,
+  },
   redis_url: process.env.REDIS_URL,
   database_url: process.env.DATABASE_URL,
   database_type: 'postgres',
 };
 
 export default defineConfig({
+  admin: {
+    backendUrl: process.env.RAILWAY_PUBLIC_DOMAIN_VALUE,
+  },
   projectConfig,
   plugins,
   modules: {},
