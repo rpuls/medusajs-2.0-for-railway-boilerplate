0f9780ac45c701ff8568646bacab48a2dba37cb6
Rasmus Puls <rasmus@triggerz.com>
2024-11-20 10:09:29 +0100

    adjust to new build path

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -48,7 +48,7 @@ const medusaConfig = {
             resolve: '@medusajs/file-local',
             id: 'local',
             options: {
-              upload_dir: 'static',
+              upload_dir: '../static',
               backend_url: `${BACKEND_URL}/static`
             }
           }
