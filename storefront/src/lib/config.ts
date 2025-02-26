9348df326d59fb52c90d1d10840ec626a14495f3
olegmichailov <olegmikhailov698@gmail.com>
2025-02-26 13:22:16 +0100

    Update config.ts

--- storefront/src/lib/config.ts
+++ storefront/src/lib/config.ts
@@ -1,7 +1,7 @@
 import Medusa from "@medusajs/js-sdk"
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
+let MEDUSA_BACKEND_URL = "https://backend-production-8122.up.railway.app"
 
 if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
