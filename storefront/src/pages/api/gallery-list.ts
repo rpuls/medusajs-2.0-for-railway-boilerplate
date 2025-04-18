ea1db0ede3d871295d8c595f4f0707f285dc75ab
olegmichailov <olegmikhailov698@gmail.com>
2025-04-18 23:55:38 +0200

    Update gallery-list.ts

--- storefront/src/pages/api/gallery-list.ts
+++ storefront/src/pages/api/gallery-list.ts
@@ -5,7 +5,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const dir = join(process.cwd(), "public/gallery")
   const files = readdirSync(dir).filter((file) =>
-    /\.(jpe?g|png|gif|webp)$/i.test(file)
+    /\.(jpe?g|png|webp)$/i.test(file)
   )
   res.status(200).json(files)
 }
