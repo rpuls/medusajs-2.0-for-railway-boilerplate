023c45c2251747307385298d2db46f4c634bc91f
olegmichailov <olegmikhailov698@gmail.com>
2025-04-18 23:31:41 +0200

    Create gallery-list.ts

---
+++ storefront/src/pages/api/gallery-list.ts
@@ -0,0 +1,11 @@
+import { readdirSync } from "fs"
+import { join } from "path"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const dir = join(process.cwd(), "public/gallery")
+  const files = readdirSync(dir).filter((file) =>
+    /\.(jpe?g|png|gif|webp)$/i.test(file)
+  )
+  res.status(200).json(files)
+}
