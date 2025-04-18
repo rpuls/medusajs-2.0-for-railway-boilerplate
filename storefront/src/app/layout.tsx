783573d62adcb13b303bc11c911e6f44da2104a5
olegmichailov <olegmikhailov698@gmail.com>
2025-04-18 20:50:52 +0200

    Update layout.tsx

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -10,13 +10,14 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <head>
+        {/* Подключение шрифта Barlow Condensed */}
         <link
           href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body className="font-sans tracking-wider text-base">
-        {props.children}
+      <body className="font-sans tracking-wide text-base antialiased">
+        <main className="relative">{props.children}</main>
       </body>
     </html>
   )
