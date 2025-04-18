4a066e0c79ad3f84c42294919661f6bc684172f2
olegmichailov <olegmikhailov698@gmail.com>
2025-04-19 01:21:30 +0200

    Update layout.tsx

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -1,19 +1,28 @@
+import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 
-import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
-import { getBaseURL } from "@lib/util/env"
+import Footer from "@modules/layout/templates/footer"
+import "styles/globals.css"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
 
-export default async function PageLayout(props: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Nav />
-      {props.children}
-      <Footer />
-    </>
+    <html lang="en" data-mode="light">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-sans tracking-wide text-base antialiased bg-white text-black">
+        <Nav />
+        <main className="relative min-h-screen">{children}</main>
+        <Footer />
+      </body>
+    </html>
   )
 }
