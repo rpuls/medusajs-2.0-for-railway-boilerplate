8b0f2c059b30f553e10be285722b3ddfd01a2abb
olegmichailov <olegmikhailov698@gmail.com>
2025-04-19 14:56:34 +0200

    Update index.tsx

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
       {/* Контент поверх видео */}
       <div className="absolute inset-0 flex flex-col justify-center items-start pl-20 z-10">
-        <h1 className="text-5xl font-[520] uppercase tracking-wide drop-shadow-md">
+        <h1 className="text-5xl font-[505] uppercase tracking-wide drop-shadow-md">
           GMORKL SPRING COLLECTION
         </h1>
 
