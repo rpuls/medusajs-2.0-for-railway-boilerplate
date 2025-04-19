fdb7f5665638cd5979788b4e5ca67ee4c36d87a3
olegmichailov <olegmikhailov698@gmail.com>
2025-04-19 20:40:03 +0200

    Update layout.tsx

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "../globals.css"
+import "../../globals.css"
 import { Inter } from "next/font/google"
 import { getCountryCode } from "@lib/localization/get-country-code"
 import Nav from "@modules/layout/templates/nav"
