a9b3ac9e82bc9df5cfd681ae22e5bc3799074f00
olegmichailov <olegmikhailov698@gmail.com>
2025-04-19 20:33:06 +0200

    Update layout.tsx

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" data-mode="light">
       <head>
-        {/* Пре-загрузка шрифта для корректного рендеринга */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -25,10 +24,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta charSet="UTF-8" />
         <title>GMORKL STORE</title>
       </head>
-      <body className="m-0 p-0 font-sans tracking-wide text-base antialiased bg-white text-[#111827]">
+      <body className="font-sans tracking-wide text-base antialiased bg-white text-[#111827]">
         <Nav />
-        {/* 👇 убрали лишние стили и отступы у main */}
-        <main className="m-0 p-0">{children}</main>
+        <main className="relative min-h-screen">{children}</main>
         <Footer />
       </body>
     </html>
