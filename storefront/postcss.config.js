5cd54cf093a1acf9275f351d85b44b7bb4d4bc6c
Rasmus Puls <14924038+rpuls@users.noreply.github.com>
2026-01-29 00:58:55 +0100

    Dependencies for better compatibility

--- storefront/postcss.config.js
+++ storefront/postcss.config.js
@@ -1,9 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},  // ← Tailwind v4 syntax
+    tailwindcss: {},
     autoprefixer: {},
   },
-};
-
-export default config;
+}
