e00ee2735c2ffbbc6ba6270db1ca4887b326a10b
rachidyekini <rachidyekin1@gmail.com>
2026-04-23 19:18:42 +0400

    test commit

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -2,7 +2,7 @@ import { loadEnv, Modules, defineConfig } from '@medusajs/utils';
 import {
   ADMIN_CORS,
   AUTH_CORS,
-  BACKEND_URL,
+  MEDUSA_BACKEND_URL,
   COOKIE_SECRET,
   DATABASE_URL,
   JWT_SECRET,
@@ -11,11 +11,11 @@ import {
   RESEND_FROM_EMAIL,
   SENDGRID_API_KEY,
   SENDGRID_FROM_EMAIL,
-  SHOULD_DISABLE_ADMIN,
+  DISABLE_MEDUSA_ADMIN,
   STORE_CORS,
   STRIPE_API_KEY,
   STRIPE_WEBHOOK_SECRET,
-  WORKER_MODE,
+  MEDUSA_WORKER_MODE,
   MINIO_ENDPOINT,
   MINIO_ACCESS_KEY,
   MINIO_SECRET_KEY,
@@ -31,7 +31,7 @@ const medusaConfig = {
     databaseUrl: DATABASE_URL,
     databaseLogging: false,
     redisUrl: REDIS_URL,
-    workerMode: WORKER_MODE,
+    workerMode: MEDUSA_WORKER_MODE,
     http: {
       adminCors: ADMIN_CORS,
       authCors: AUTH_CORS,
@@ -46,41 +46,47 @@ const medusaConfig = {
     }
   },
   admin: {
-    backendUrl: BACKEND_URL,
-    disable: SHOULD_DISABLE_ADMIN,
+    backendUrl: MEDUSA_BACKEND_URL,
+    disable: DISABLE_MEDUSA_ADMIN,
   },
   modules: [
-    {
-      key: Modules.FILE,
-      resolve: '@medusajs/file',
-      options: {
-        providers: [
-          ...(MINIO_ENDPOINT && MINIO_ACCESS_KEY && MINIO_SECRET_KEY ? [{
-            resolve: './src/modules/minio-file',
-            id: 'minio',
-            options: {
-              endPoint: MINIO_ENDPOINT,
-              accessKey: MINIO_ACCESS_KEY,
-              secretKey: MINIO_SECRET_KEY,
-              bucket: MINIO_BUCKET // Optional, default: medusa-media
-            }
-          }] : [{
-            resolve: '@medusajs/file-local',
-            id: 'local',
-            options: {
-              upload_dir: 'static',
-              backend_url: `${BACKEND_URL}/static`
-            }
-          }])
-        ]
-      }
-    },
+    // {
+    //   key: Modules.FILE,
+    //   resolve: '@medusajs/file',
+    //   options: {
+    //     providers: [
+    //       ...(MINIO_ENDPOINT && MINIO_ACCESS_KEY && MINIO_SECRET_KEY ? [{
+    //         resolve: './src/modules/minio-file',
+    //         id: 'minio',
+    //         options: {
+    //           endPoint: MINIO_ENDPOINT,
+    //           accessKey: MINIO_ACCESS_KEY,
+    //           secretKey: MINIO_SECRET_KEY,
+    //           bucket: MINIO_BUCKET // Optional, default: medusa-media
+    //         }
+    //       }] : [{
+    //         resolve: '@medusajs/file-local',
+    //         id: 'local',
+    //         options: {
+    //           upload_dir: 'static',
+    //           MEDUSA_BACKEND_URL: `${MEDUSA_BACKEND_URL}/static`
+    //         }
+    //       }])
+    //     ]
+    //   }
+    // },
     ...(REDIS_URL ? [{
       key: Modules.EVENT_BUS,
       resolve: '@medusajs/event-bus-redis',
       options: {
         redisUrl: REDIS_URL
       }
+    },
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: REDIS_URL,
+      },
     },
     {
       key: Modules.WORKFLOW_ENGINE,
